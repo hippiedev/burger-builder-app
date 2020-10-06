d5fface1585fa71192f99526f764693ce30670dc
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2020-10-06 20:20:08 +0100

    feat: Navigation

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import burgerlogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css'
+
+const Logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerlogo} alt="myBurger" />
+    </div>
+);
+
+export default Logo
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo'
+
+const Toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <Logo/>
+        <nav>
+            <ul>
+                j
+            </ul>
+        </nav>
+    </header>
+
+)
+
+export default Toolbar
