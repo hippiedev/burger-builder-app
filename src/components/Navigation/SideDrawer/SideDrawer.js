4c8fe2b8dc664f8956f59489771d22b6418b1658
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-01-08 23:24:16 +0100

    changes

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,7 @@ const SideDrawer = (props) => {
     return (
         <Aux>
         <Backdrop show={props.open} clicked={props.closed}/>
-        <div className={attachedClasses.join(' ')}>
+        <div className={attachedClasses.join(' ')} onClick={props.closed}>
             <Logo height="11%"/>
             <nav>
                 <NavigationItems isAuthenticated={props.isAuth}/>
